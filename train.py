9c9b737bdd09fcf5e7d2cc6b572b8ed6c84fd99c
Carro <robertmyers@hey.com>
2023-09-08 02:13:02 -0500

    added revision

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ def main(config: DictConfig):
         print('building reference model')
         reference_model_dtype = getattr(torch, config.model.reference_dtype)
         reference_model = transformers.AutoModelForCausalLM.from_pretrained(
-            config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=reference_model_dtype, **model_kwargs)
+            config.model.name_or_path, cache_dir=get_local_dir(config.local_dirs), low_cpu_mem_usage=True, torch_dtype=reference_model_dtype, revision=config.model.revision, **model_kwargs)
         disable_dropout(reference_model)
     else:
         reference_model = None
